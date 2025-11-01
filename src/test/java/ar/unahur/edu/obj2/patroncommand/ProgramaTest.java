be3cd712518e81ce650a17066bcd55552b6ed2da
LucaYT <gamerytluca@gmail.com>
2025-11-01 16:50:26 -0300

    Prueba

--- src/test/java/ar/unahur/edu/obj2/patroncommand/ProgramaTest.java
+++ src/test/java/ar/unahur/edu/obj2/patroncommand/ProgramaTest.java
@@ -7,7 +7,12 @@ import org.junit.jupiter.api.Test;
 
 import ar.unahur.edu.obj2.patroncommand.Invoker.Programa;
 import ar.unahur.edu.obj2.patroncommand.microcontrolador.Microcontrolador;
+import ar.unahur.edu.obj2.patroncommand.operaciones.Add;
+import ar.unahur.edu.obj2.patroncommand.operaciones.Lod;
+import ar.unahur.edu.obj2.patroncommand.operaciones.Lodv;
 import ar.unahur.edu.obj2.patroncommand.operaciones.Nop;
+import ar.unahur.edu.obj2.patroncommand.operaciones.Str;
+import ar.unahur.edu.obj2.patroncommand.operaciones.Swap;
 
 public class ProgramaTest {
     private Programa p = new Programa();
@@ -28,5 +33,31 @@ public class ProgramaTest {
         p.ejecutar(m);
 
         assertEquals(3, m.getProgramCounter());
+    }
+
+    @Test
+    void sumar20Mas17YObtener37AcumuladorA(){
+        p.addOperacion(new Lodv(20));
+        p.addOperacion(new Swap());
+        p.addOperacion(new Lodv(17));
+        p.addOperacion(new Add());
+
+        p.ejecutar(m);
+
+        assertEquals(37, m.getAcumuladorA());
+        assertEquals(4, m.getProgramCounter());
+    }
+
+    @Test
+    void sumar2Mas8Mas5EnAcumuladorA(){
+        p.addOperacion(new Lodv(2));
+        p.addOperacion(new Str(0));
+        p.addOperacion(new Lodv(8));
+        p.addOperacion(new Swap());
+        p.addOperacion(new Lodv(5));
+        p.addOperacion(new Add());
+        p.addOperacion(new Swap());
+        p.addOperacion(new Lod(0));
+        p.addOperacion(new Add());
     }
 }
